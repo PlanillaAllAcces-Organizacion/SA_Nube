a4cdb0224641c1144e7d03a972772b04cf5ad59b
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-05-06 15:05:14 -0300

    Se creo el controlador Archivos

---
+++ GRUPO_01_SubirArchivosNube.AppWebMVC/Controllers/ArchivosController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GRUPO_01_SubirArchivosNube.AppWebMVC.Controllers
+{
+    public class ArchivosController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
